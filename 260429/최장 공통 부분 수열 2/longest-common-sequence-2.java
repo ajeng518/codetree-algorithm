15073dc6a30f1f43e96099b93f7544f5424d1a85
여아정 <elegant518@gmail.com>
2026-04-29 15:55:03 +0900

    Title: 최장 공통 부분 수열 2, Time: 0ms, Memory: 0MB, Status: Memory Limit Exceed - Codetree

---
+++ 260429/최장 공통 부분 수열 2/longest-common-sequence-2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        
+        String a =" "+br.readLine();
+        String b =" "+br.readLine();
+        int n = a.length()-1;
+        int m = b.length()-1;
+
+        StringBuilder[][] dp = new StringBuilder[n+1][m+1];
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=m;j++)
+                dp[i][j]=new StringBuilder();
+
+        if(a.charAt(1) == b.charAt(1)){
+            dp[1][1].append(a.charAt(1));
+        }
+
+        for(int i=2; i<=n; i++){
+            if(a.charAt(i) == b.charAt(1)){
+                dp[i][1].append(a.charAt(i));
+            }else dp[i][1].append(dp[i-1][1]);
+        }
+
+        for(int j=2; j<=m; j++){
+            if(a.charAt(1) == b.charAt(j)){
+                dp[1][j].append(a.charAt(1));
+            }else dp[1][j].append(dp[1][j-1]);
+        }
+
+        for(int i=2; i<=n; i++){
+            for(int j=2; j<=m; j++){
+                if(a.charAt(i) == b.charAt(j)){
+                    dp[i][j]=new StringBuilder(dp[i-1][j-1].toString());
+                    dp[i][j].append(a.charAt(i));
+                }else{
+                    int left =dp[i-1][j].length();
+                    int right = dp[i][j-1].length();
+
+                    if(left <= right){
+                        dp[i][j]=new StringBuilder(dp[i-1][j].toString());
+                        dp[i][j].append(a.charAt(i));
+                    }else{
+                        dp[i][j]=new StringBuilder(dp[i][j-1].toString());
+                        dp[i][j].append(a.charAt(i));
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
